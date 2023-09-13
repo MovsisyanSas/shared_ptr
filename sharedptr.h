c634a7b81a674c71f6ff1e5a74a9213abeb60f27
MovsisyanSas <sasunmov07@gmail.com>
2023-09-14 00:07:57 +0400

    changig deletion functionality, using pointer in nested class

--- sharedptr.h
+++ sharedptr.h
@@ -1,11 +1,16 @@
 #pragma once
 
+
 template <typename T>
 class sharedptr {
+	class CenterBlock;
+	CenterBlock* cb = nullptr;
+private:
 	class CenterBlock {
 		int count;
+		T* m_ptr = nullptr;
 	public:
-		CenterBlock() {
+		CenterBlock(T* ptr) : m_ptr(ptr) {
 			count = 1;
 		}
 		int get_count() {
@@ -17,88 +22,108 @@ class sharedptr {
 		int decrement() {
 			return --count;
 		}
+		void set_ptr(T* ptr) {
+			m_ptr = ptr;
+		}
+		T* get_ptr() {
+			if (m_ptr == nullptr) {
+				return nullptr;
+			}
+			return m_ptr;
+		}
+		void deleteptr() {
+			if (--count == 0) {
+				delete m_ptr;
+			}
+		}
+		~CenterBlock() {
+			deleteptr();
+		}
 	};
-private:
-	CenterBlock* counter = nullptr;
-	T* s_ptr = nullptr;
 public:
 	sharedptr() = default;
 	explicit sharedptr(T* obj) {
-		counter = new CenterBlock();
-		s_ptr = obj;
+		cb = new CenterBlock(obj);
 	}
 	sharedptr(const sharedptr& obj) {
-		this->counter = obj.counter;
-		this->counter->increment();
-		this->s_ptr = obj.s_ptr;
+		this->cb = obj.cb;
+		this->cb->increment();
+		this->cb->set_ptr(obj.cb->get_ptr());
 	}
 	sharedptr& operator=(const sharedptr& obj) {
 		if (this != &obj) {
-			if (s_ptr != nullptr) {
-				if (counter->decrement() == 0) {
-					delete counter;
-					delete s_ptr;
+			if (cb != nullptr)
+			{
+				if (cb->get_ptr() != nullptr) {
+					cb->deleteptr();
+					delete this->cb;
 				}
 			}
-			counter = obj.counter;
-			counter->increment();
-			s_ptr = obj.s_ptr;
+			this->cb = obj.cb;
+			this->cb->increment();
+			this->cb->set_ptr(obj.cb->get_ptr());
 		}
 		return *this;
 	}
 	sharedptr(sharedptr&& obj) noexcept {
-		this->counter = obj.counter;
-		this->s_ptr = obj.s_ptr;
-		obj.counter = nullptr;
-		obj.s_ptr = nullptr;
+		this->cb = obj.cb;
+		this->cb->set_ptr(obj.cb->get_ptr());
+		obj.cb->set_ptr(nullptr);
+		obj.cb = nullptr;
 	}
-	sharedptr& operator=(sharedptr&& obj) noexcept{
+	sharedptr& operator=(sharedptr&& obj) noexcept {
 		if (this != &obj) {
-			if (s_ptr != nullptr) {
-				if (counter->decrement() == 0) {
-					delete counter;
-					delete s_ptr;
+			if (cb != nullptr)
+			{
+				if (cb->get_ptr() != nullptr) {
+					cb->deleteptr();
 				}
 			}
-			counter = obj.counter;
-			s_ptr = obj.s_ptr;
-			obj.counter = nullptr;
-			obj.s_ptr = nullptr;
+			this->cb = obj.cb;
+			this->cb->set_ptr(obj.cb->get_ptr());
+			obj.cb->set_ptr(nullptr);
+			obj.cb = nullptr;
 		}
 		return *this;
 	}
 	~sharedptr() {
-		if (s_ptr != nullptr && counter->decrement() == 0) {
-			delete counter;
-			delete s_ptr;
-			counter = nullptr;
-			s_ptr = nullptr;
+		if (cb != nullptr)
+		{
+			if (cb->get_ptr() != nullptr) {
+				cb->deleteptr();
+				cb->set_ptr(nullptr);
+				cb = nullptr;
+			}
 		}
 	}
 public:
 	int use_count() {
-		if (counter == nullptr) { return 0; }
-		return counter->get_count();
+		if (cb == nullptr) { return 0; }
+		return cb->get_count();
 	}
 	T* get() {
-		return s_ptr;
+		if (cb == nullptr) { return nullptr; }
+		return cb->get_ptr();
 	}
 	T* operator ->() {
-		return s_ptr;
+		if (cb == nullptr) { return nullptr; }
+		return cb->get_ptr();
 	}
 	T operator *() {
-		return * s_ptr;
+		if (cb == nullptr) { return INT_MIN; }
+		return *(cb->get_ptr());
 	}
 	void reset(T* obj = nullptr) {
-		if (s_ptr != nullptr && counter->get_count() == 1) {
-			delete counter;
-			delete s_ptr;
-			counter = nullptr;
-			s_ptr = nullptr;
-		}
-		else {
-			s_ptr = obj;
-			counter = new CenterBlock();
+		if (cb != nullptr) {
+			if (cb->get_ptr() != nullptr) {
+				cb->deleteptr();
+				cb->set_ptr(nullptr);
+				cb = nullptr;
+			}
+			else {
+				cb->set_ptr(obj);
+				cb = new CenterBlock();
+			}
 		}
 	}
 
